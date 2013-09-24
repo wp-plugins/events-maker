73e8a4a10b6e3b59456817cea42777d25e11e934
dfactory <dfactory@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-24 10:36:14 +0000

    Update to version 1.0.2.3
    git-svn-id: https://plugins.svn.wordpress.org/events-maker/trunk@777674 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- events-maker.php
+++ events-maker.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Events Maker
 Description: Events Maker is an easy to use but flexible events management plugin made the WordPress way.
-Version: 1.0.2.2
+Version: 1.0.2.3
 Author: dFactory
 Author URI: http://www.dfactory.eu/
 Plugin URI: http://www.dfactory.eu/plugins/events-maker/

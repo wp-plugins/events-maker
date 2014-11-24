b6bbcdabcf4a66c40ffe171ae588308a2b38fbf3
dfactory <dfactory@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-24 12:43:01 +0000

    Tagging version 1.3.1
    git-svn-id: https://plugins.svn.wordpress.org/events-maker/tags/1.3.1@1031728 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/content-single-event.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * The template for displaying event content in the single-event.php template
+ *
+ * Override this template by copying it to yourtheme/content-single-event.php
+ *
+ * @author 	Digital Factory
+ * @package Events Maker/Templates
+ * @since 	1.1.0
+ */
+ 
+if (!defined('ABSPATH')) exit; // Exit if accessed directly
+
+// Extra event classes
+$classes = apply_filters('em_single_event_classes', array('hcalendar'));
+
+?>
+
+	<article id="post-<?php the_ID(); ?>" <?php post_class($classes); ?>>
+		
+		<?php
+		/**
+		 * em_before_single_event hook
+		 * 
+		 * @hooked em_display_single_event_thumbnail - 10
+		 */
+		do_action('em_before_single_event');
+		?>
+	
+	    <header class="entry-header">
+	    	
+	    	<?php
+			/**
+			 * em_before_single_event_title hook
+			 * 
+			 * @hooked em_display_event_categories - 10
+			 */
+			do_action ('em_before_single_event_title');
+			?>
+			
+			<h1 class="entry-title summary">
+				
+				<?php the_title(); ?>
+				
+			</h1>
+			
+			<?php
+			/**
+			 * em_after_single_event_title hook
+			 * 
+			 * @hooked em_display_single_event_meta - 10
+			 * @hooked em_display_event_locations - 20
+			 * @hooked em_display_event_organizers - 30
+			 * @hooked em_display_google_map - 40
+			 * @hooked em_display_event_tickets - 50
+			 */
+			do_action ('em_after_single_event_title');
+			?>
+
+	    </header>
+	    
+	    <?php
+		/**
+		 * em_single_event_content hook
+		 * 
+		 * @hooked em_display_event_content - 10
+		 */
+		do_action('em_single_event_content');
+		?>
+	    
+	    <?php
+		/**
+		 * em_after_single_event hook
+		 */
+		do_action('em_after_single_event');
+		?>
+
+	</article>
